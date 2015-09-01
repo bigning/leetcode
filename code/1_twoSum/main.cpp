c27fa24be358b6911e940f343dc5ae5c95fe4f40
WangNing <wangning@172-16-13-253.DYNAPOOL.NYU.EDU>
2015-09-01 17:41:42 -0400

    test

--- code/1_twoSum/main.cpp
+++ code/1_twoSum/main.cpp
@@ -1,80 +1,10 @@
-#include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
-
-
+using std::vector;
 
 class Solution {
 public:
-
-	struct MyStruct
-	{
-		int value, index;
-		bool operator<(const MyStruct & str) const
-		{
-			return value < str.value;
-		}
-	};
-
-	vector<int> twoSum(vector<int> &numbers, int target) {
-
-		vector<int> res;
-		MyStruct tmp;
-		vector<MyStruct> my_vec;
-		for (int i = 0; i < numbers.size(); i++)
-		{
-			tmp.value = numbers[i];
-			tmp.index = i + 1;
-			my_vec.push_back(tmp);
-		}
-		sort(my_vec.begin(), my_vec.end());
-
-		int i = 0, j = my_vec.size() - 1;
-		while (1)
-		{
-			if (my_vec[i].value + my_vec[j].value == target)
-			{
-				if (my_vec[i].index < my_vec[j].index)
-				{
-					res.push_back(my_vec[i].index);
-					res.push_back(my_vec[j].index);
-					return res;
-				}
-				else
-				{
-					res.push_back(my_vec[j].index);
-					res.push_back(my_vec[i].index);
-					return res;
-				}
-			}
-			if (my_vec[i].value + my_vec[j].value < target)
-			{
-				i++;
-			}
-			else
-			{
-				j--;
-			}
-		}
-		return res;
-	}
-
-
-};
-
-int main()
-{
-	Solution s;
-	vector<int> test_vec;
-	test_vec.push_back(11);
-	test_vec.push_back(7);
-	test_vec.push_back(10);
-	test_vec.push_back(15);
-
-	vector<int> res;
-	res = s.twoSum(test_vec, 22);
-	cout<<res[0]<<endl<<res[1]<<endl;
-	return 0;
+    vector<int> twoSum(vector<int>& nums, int target) {
+    }
 }
